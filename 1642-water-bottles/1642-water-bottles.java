63e7b17192fb6fcb4cc552b041df95389a963bfb
Pr17-git <gkpraveenraj17@gmail.com>
2025-10-01 10:53:32 +0530

    Time: 0 ms (100%), Space: 40.3 MB (86.01%) - LeetHub

--- 1642-water-bottles/1642-water-bottles.java
+++ 1642-water-bottles/1642-water-bottles.java
@@ -2,24 +2,24 @@ class Solution {
     public int numWaterBottles(int b, int t) {
         if(b<t) return b;
         int y=b;
-        if(b%t==0){
+        // if(b%t==0){
+        //     while(b>=t){
+        //     int k=b%t;
+        //     if(k==0){
+        //         y+=b/t;
+        //         b=b/t;
+        //     }else{
+        //         System.out.println("oo");
+        //         y+=b/t;
+        //         b=(b%t)+(b/t);
+        //     }
+        // }
+        // }else{
             while(b>=t){
-            int k=b%t;
-            if(k==0){
-                y+=b/t;
-                b=b/t;
-            }else{
-                System.out.println("oo");
                 y+=b/t;
                 b=(b%t)+(b/t);
             }
-        }
-        }else{
-            while(b>=t){
-                y+=b/t;
-                b=(b%t)+(b/t);
-            }
-        }
+        // }
         return y;
     }
 }
